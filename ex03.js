090ecfdf4142af7d8a38b295e4829ada7426038c
Arnofraye <aboudissa@edenschool.fr>
2026-01-15 09:23:37 +0100

    feat ex03

---
+++ ex03.js
@@ -0,0 +1,10 @@
+function max2(a, b) {
+	if (a > b) {
+		return a;
+	} else {
+		return b;
+	}
+}
+
+console.log(max2(1, 3)); // Résultat attendu: 3
+console.log(max2(-1, -2)); // Résultat attendu: -1
