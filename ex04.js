416d9e1cafd6d101c8461e197492d72347a13d00
Arnofraye <aboudissa@edenschool.fr>
2026-01-15 09:31:44 +0100

    feat: ex04

---
+++ ex04.js
@@ -0,0 +1,11 @@
+function premierElement(tab) {
+	if (tab.length == 0) {
+		return null;
+	} else {
+		return tab[0];
+	}
+}
+
+console.log(premierElement([1, 2, 3])); // Résultat attendu: 1
+console.log(premierElement(["a", "b", "c"])); // Résultat attendu: "a"
+console.log(premierElement([])); // Résultat attendu: null
